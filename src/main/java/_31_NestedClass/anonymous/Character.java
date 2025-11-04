b4fb1764f2d03c3a105431e37383fdf113aadd9f
박정빈 <jeongbin@bagjeongbin-ui-MacBookPro.local>
2025-11-04 21:45:03 +0900

    new:  nestedclass, enum

---
+++ src/main/java/_31_NestedClass/anonymous/Character.java
@@ -0,0 +1,40 @@
+package _31_NestedClass.anonymous;
+
+// 게임캐릭터
+public abstract class Character { // 추상메서드는 클래스 앞에도 abstract 사용해야함
+    // 추상(abstract) : 명세만 있고, 구체적인 구현은 없는 것
+    // 추상클래스: 추상메서드를 하나 이상 포함하는 클래스
+    // 추상메서드: 메서드의 시그니처만 정의하고, 구현(중괄호)를 생략
+    // 상속받는 클래스에서는, 반드시 부모의 추상메서드를 오버라이딩 해줘야 한다.
+
+    protected String name; // 이름
+    protected int Hp; // 체력
+    protected int attackDamage; // 공격력
+
+    public Character(String name, int hp, int attackDamage) {
+        this.name = name;
+        this.Hp = hp;
+        this.attackDamage = attackDamage;
+    }
+
+    // 각 캐릭터마다 공격하는 방식은 달라져야 한다.
+    // 추상메서드 -> {} 중괄호 x
+    public abstract void attack(Character target);
+
+    public void showStatus() {
+        System.out.println(name + " 체력: " + Hp);
+    }
+
+    // 데미지를 받는 공통 메서드
+    public void receiveDamage(int damage) {
+        this.Hp -= damage;
+        if (this.Hp <= 0 ){
+            this.Hp = 0; // this 생략가능할까? o
+            System.out.println(name + "가 쓰러졌습니다.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/_31_NestedClass/nested/Main.java
@@ -0,0 +1,50 @@
+package _31_NestedClass.nested;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        내부 클래스(클래스 안에 클래스)
+        1. 내부클래스 - 권장하지 x
+        내부클래스의 객체는 반드시 외부 클래스의 객체 안에서만 생성될 수 있다.
+        -> 내부인스턴스는 항상 바깥인스턴스를 참조하고 있다.
+        바깥인스턴스는 사용되지 않고, 내부인스턴스만 사용되는 상황이 오더라도
+        바깥인스턴스는 정리되지 않고(참조가 있기 때문에) 결과적으로 메모리 누수로 이어진다.
+
+        잘 설계하려면?
+        1. 내부인스턴스의 생성을 바깥클래스의 메서드를 통해서만 생성하게 만들어야 한다.
+        2. 내부인스턴스들을 바깥인스턴스의 필드로 저장해야 한다.(private)
+        3. 내부클래스의 생성자를 private로 선언해줘야 한다.
+         */
+
+        // 정적 내부 클래스 - 사실상 바깥클래스와 독립적인 클래스
+        // 다만 바깥클래스의 private 메서드와 필드에 접근할 수 있다.
+
+        // 생성자와 달리 필드초기화 할때 순서가 상관 없어진다.(구성요소만 알면 된다)
+        Member member1 = Member // 클래스참조
+                .builder() // static 메서드 호출 -> 결과: Builder 객체 생성
+                .name("홍길동") // 생성한 Builder 객체에 set
+                .age(20) //Builder 객체에 set
+                .email("java@naver.com") //Builder 객체에 set
+                .build(); // set 해준 Builder 객체의 필드를 그대로 member 객체로 이동시켜 생성
+
+        // 내부 클래스를 참조하기 위해 바깥 User 객체를 생성
+            User user1 = new User.Builder()
+                .username("사용자1")
+                .name("홍길동")
+                .address("부산시")
+                .email("java@naver.com")
+                .build();
+
+
+            LombokUser lombokUser = LombokUser
+                    .builder()
+                    .username("자바맨")
+                    .name("김길동")
+                    .email("java@naver.com")
+                    .address("부산시")
+                    .build();
+
+        // 롬복이 알아서 만들어준 toString()
+        System.out.println(lombokUser);
+    }
+}
